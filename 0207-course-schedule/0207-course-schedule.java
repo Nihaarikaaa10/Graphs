01efccfe68c10f48fd42005ab811a00876dd2937
Nihaarikaaa10 <120477368+Nihaarikaaa10@users.noreply.github.com>
2024-06-11 22:07:36 +0530

    Time: 6 ms (46.82%), Space: 45.4 MB (30.55%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int v, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<v;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<pre.length;i++){
+            adj.get(pre[i][0]).add(pre[i][1]);
+        }
+        int[] inDeg = new int[v];
+        for(int i=0;i<v;i++){
+            for(int item: adj.get(i)){
+                inDeg[item]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<v;i++){
+            if(inDeg[i]==0) q.add(i);
+        }
+        List<Integer> al = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+            al.add(node);
+            for(int it: adj.get(node)){
+                inDeg[it]--;
+                if(inDeg[it]==0) q.add(it);
+            }
+        }
+        if(al.size()==v) return true;
+        return false;
+    }
+}
